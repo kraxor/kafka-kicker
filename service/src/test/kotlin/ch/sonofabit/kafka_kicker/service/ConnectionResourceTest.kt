50d1602880ed918977789e62c73185879f6005fe
kraxor <bence@sonofabit.ch>
2023-06-04 12:30:39 +0100

    refactor testing

--- service/src/test/kotlin/ch/sonofabit/kafka_kicker/service/ConnectionResourceTest.kt
+++ service/src/test/kotlin/ch/sonofabit/kafka_kicker/service/ConnectionResourceTest.kt
@@ -1,48 +1,33 @@
 package ch.sonofabit.kafka_kicker.service
 
-import io.quarkus.hibernate.reactive.panache.Panache
+import ch.sonofabit.kafka_kicker.service.util.transactional
 import io.quarkus.test.junit.QuarkusTest
 import io.quarkus.test.vertx.RunOnVertxContext
 import io.quarkus.test.vertx.UniAsserter
-import io.quarkus.test.vertx.UniAsserterInterceptor
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
-import io.smallrye.mutiny.Uni
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.Test
-import java.util.function.Supplier
 
 @QuarkusTest
 class ConnectionResourceTest {
     @Test
     @RunOnVertxContext
     fun `returns 200 when entity is found`(asserter: UniAsserter) {
-        var id: Long? = null
+        val connection = Connection().apply {
+            name = "test"
+            bootstrapServers = "broker:1337"
+        }
 
         asserter.transactional {
-            val connection = Connection().apply {
-                name = "test"
-                bootstrapServers = "broker:1337"
-            }
-
             assertNotNull { connection.persistAndFlush() }
-            execute { id = connection.id!! }
-
         }.execute {
             When {
-                get("/connection/${id}")
+                get("/connection/${connection.id}")
             }.Then {
                 statusCode(200)
                 body(`is`("{\"name\":\"test\",\"bootstrapServers\":\"broker:1337\"}"))
             }
         }
     }
-
-    class TransactionalAsserter(asserter: UniAsserter) : UniAsserterInterceptor(asserter) {
-        override fun <T> transformUni(uniSupplier: Supplier<Uni<T>>): Supplier<Uni<T>> =
-            Supplier { Panache.withSession { Panache.withTransaction(uniSupplier) } }
-    }
-
-    private fun UniAsserter.transactional(block: TransactionalAsserter.() -> Unit) =
-        TransactionalAsserter(this).block().let { this }
 }
