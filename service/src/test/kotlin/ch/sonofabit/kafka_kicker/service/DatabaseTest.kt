a2b6eac033c84c8b2d0c8e024c3d099d5a8cf9d3
kraxor <bence@sonofabit.ch>
2023-06-11 10:13:37 +0100

    cleanup

--- service/src/test/kotlin/ch/sonofabit/kafka_kicker/service/DatabaseTest.kt
+++ service/src/test/kotlin/ch/sonofabit/kafka_kicker/service/DatabaseTest.kt
@@ -1,61 +1,23 @@
 package ch.sonofabit.kafka_kicker.service
 
-import io.quarkus.hibernate.reactive.panache.Panache
+import ch.sonofabit.kafka_kicker.service.util.transactional
+import io.kotest.matchers.shouldNotBe
 import io.quarkus.test.junit.QuarkusTest
 import io.quarkus.test.vertx.RunOnVertxContext
 import io.quarkus.test.vertx.UniAsserter
-import io.quarkus.test.vertx.UniAsserterInterceptor
-import io.smallrye.mutiny.Uni
 import org.junit.jupiter.api.Test
-import java.util.function.Supplier
 
 @QuarkusTest
 class DatabaseTest {
     @Test
     @RunOnVertxContext
-    fun `entities can be persisted`(asserter: UniAsserter) {
-//        TransactionalUniAsserterInterceptor(asserter).run {
-//            val connection = Connection().apply {
-//                name = "test"
-//                bootstrapServers = "broker:1337"
-//            }
-//        }
-
-
+    fun `entities can be persisted`(asserter: UniAsserter): Unit = asserter.run {
         val connection = Connection().apply {
             name = "test"
             bootstrapServers = "broker:1337"
         }
 
-//        connection.persist<Connection>()
-
-        asserter.transactional {
-            assertNotNull { connection.persist() }
-        }
-
-
-//        asserter.transactional {}
+        transactional { assertNotNull { connection.persist() } }
+        execute { connection.id shouldNotBe null }
     }
-
-    //    @WithSession
-    class TransactionalAsserter(asserter: UniAsserter) : UniAsserterInterceptor(asserter) {
-        override fun <T> transformUni(uniSupplier: Supplier<Uni<T>>): Supplier<Uni<T>> =
-            Supplier {
-//                Panache.withSession {
-                Panache.withTransaction(uniSupplier)
-//                }
-            }
-    }
-
-    class TransactionalUniAsserterInterceptor(asserter: UniAsserter) : UniAsserterInterceptor(asserter) {
-
-        //        @Transactional
-        override fun <T> transformUni(uniSupplier: Supplier<Uni<T>>): Supplier<Uni<T>> {
-            // Assert/execute methods are invoked within a database transaction
-            return Supplier { Panache.withTransaction(uniSupplier) }
-        }
-    }
-
-    private fun UniAsserter.transactional(block: TransactionalAsserter.() -> Unit): Unit =
-        TransactionalAsserter(this).block()
 }
