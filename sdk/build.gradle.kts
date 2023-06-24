f0a25de781833d219a7f7ec8002ad2d426c17775
kraxor <bence@sonofabit.ch>
2023-06-24 14:07:06 +0100

    generate kotlin and typescript SDKs

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
 plugins {
     kotlin("jvm")
     id("org.openapi.generator") version "6.6.0"
@@ -6,11 +8,19 @@ plugins {
 group = "ch.sonofabit.kafka.kicker"
 version = "1.0.0-SNAPSHOT"
 
-openApiGenerate {
-    generatorName.set("kotlin")
-    inputSpec.set("$projectDir/openapi/openapi.yaml")
-    skipValidateSpec.set(true) // FIXME (upstream) the generated schema is missing some descriptions
-}
+
+fun TaskContainer.registerClientSdk(name: String, config: GenerateTask.() -> Unit) =
+    register("generateSdkClient-$name", GenerateTask::class.java) {
+        group = "SDK clients"
+        generatorName.set(name)
+        inputSpec.set("$projectDir/openapi/openapi.yaml")
+        outputDir.set("$projectDir/clients/$name")
+        skipValidateSpec.set(true) // FIXME (upstream) the generated schema is missing some descriptions
+        config()
+    }
+
+tasks.registerClientSdk("kotlin") {}
+tasks.registerClientSdk("typescript-fetch") {}
 
 repositories {
     mavenCentral()
