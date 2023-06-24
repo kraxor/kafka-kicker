66ffc6621a51cef2c1348f98a52dc823f0d3300f
Bence Bálint <bence@sonofabit.ch>
2023-06-24 11:04:10 +0100

    Openapi (#7)
    * add openapi dependency
    
    * add IDEA run config for docker compose
    
    * update readme
    
    * fix typo
    
    * update readme with version info
    
    * update idea config
    
    * reorganise dependencies

--- README.md
+++ README.md
@@ -2,7 +2,52 @@
 
 # kafka-kicker
 
-This is a tool for breaking and fixing your Kafka setup in reproducible ways.
+A tool for breaking and fixing your Kafka setup in reproducible ways.
+
+Tech stack TLDR:
+* Gradle 8.1.1
+* Kotlin 1.8.10
+* Apache Kafka 3.2.3
+* Quarkus 3.0.3.Final
+    * RESTEasy Reactive
+    * Hibernate Reactive REST Data with Panache
+    * Smallrye OpenAPI (OpenAPI V3)
+* Postgres (latest alpine)
+* Docker (Compose V2)
+
+# Quick Start
+
+| mode                                      | IntelliJ             | Gradle        | `QUARKUS_PROFILE` | External Services    |
+|-------------------------------------------|----------------------|---------------|-------------------|----------------------|
+| [development](#development)               | `DEV [quarkusDev]`   | `quarkusDev`  | `dev`             | provided (Quarkus)   |
+| [continuous testing](#continuous-testing) | `TEST [quarkusTest]` | `quarkusTest` | `test`            | provided (Quarkus)   |
+| [production](#production)                 | `PROD [quarkusDev]`  | `quarkusDev`  | `prod`            | `docker-compose.yml` |   
+
+All profiles use the default debugger port 5005.
+
+### Development
+
+Standard Quarkus Dev mode with live reloading, Dev UI and Dev Services enabled. Requires docker daemon.
+
+Start using IntelliJ `DEV [quarkusDev]` or from the terminal `$ ./gradlew quarkusDev`
+
+Dev UI: http://localhost:8080/q/dev
+
+Swagger UI: http://localhost:8080/q/swagger-ui
+
+### Continuous testing
+
+Standard Quarkus Test mode with continuous testing. Requires docker daemon.
+
+Start using IntelliJ `TEST [quarkusTest]` or from the terminal `$ ./gradlew quarkusTest`
+
+### Production
+
+Essentially `quarkusDev` but uses the services defined in `docker-compose.yml` instead of Quarkus Dev Services.
+This is temporary, until a proper production build is configured.
+
+Start using IntelliJ `PROD [quarkusDev]` or the
+terminal `$ docker compose up -d && QUARKUS_PROFILE=prod ./gradlew quarkusDev`
 
 # License
 
--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -13,32 +13,36 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    // internal
+    implementation(project(":lib"))
+
+    // Quarkus / core
     implementation(enforcedPlatform("$quarkusPlatformGroupId:$quarkusPlatformArtifactId:$quarkusPlatformVersion"))
     implementation("io.quarkus:quarkus-arc")
     implementation("io.quarkus:quarkus-config-yaml")
+    testImplementation("io.quarkus:quarkus-junit5")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
+    testImplementation("io.quarkus:quarkus-test-vertx")
 
+    // Smallrye Reactive Messaging
     implementation("io.quarkus:quarkus-smallrye-reactive-messaging")
     implementation("io.quarkus:quarkus-smallrye-reactive-messaging-kafka")
 
+    // REST / Hibernate / Postgres
+    implementation("io.quarkus:quarkus-smallrye-openapi")
     implementation("io.quarkus:quarkus-resteasy-reactive")
     implementation("io.quarkus:quarkus-resteasy-reactive-jackson")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.quarkus:quarkus-hibernate-reactive-rest-data-panache")
     implementation("io.quarkus:quarkus-hibernate-reactive-panache-kotlin")
-//    implementation("io.quarkus:quarkus-hibernate-reactive")
     implementation("io.quarkus:quarkus-reactive-pg-client")
+    testImplementation("io.rest-assured:kotlin-extensions")
 
+    // AWS
     implementation(enforcedPlatform("$quarkusPlatformGroupId:quarkus-amazon-services-bom:$quarkusPlatformVersion"))
     implementation("io.quarkiverse.amazonservices:quarkus-amazon-ssm")
     implementation("io.quarkiverse.amazonservices:quarkus-amazon-secretsmanager")
-
-    implementation(project(":lib"))
-
-    testImplementation("io.rest-assured:kotlin-extensions")
-    testImplementation("io.quarkus:quarkus-junit5")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
-    testImplementation("io.quarkus:quarkus-test-vertx")
 }
 
 group = "ch.sonofabit.kafka-kicker.backend.service"
