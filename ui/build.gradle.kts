a25b3922b2d2d8d638941886b66682b023275b82
kraxor <bence@sonofabit.ch>
2023-07-05 18:27:06 +0100

    cleanup

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -1,9 +1,3 @@
-import com.github.gradle.node.npm.task.NpmTask
-
 plugins {
     id("com.github.node-gradle.node") version "5.0.0"
-}
-
-tasks.register("npmStart", NpmTask::class.java) {
-    args.set(listOf("start"))
 }
